7374252bc85d116ae9e5fec18b11c03697e28ee2
delma <sdy2856@gmail.com>
2020-03-22 21:22:46 +0900

    Feat: Judge ID is Valid

--- SignUp/SignUp/Controller/ViewController.swift
+++ SignUp/SignUp/Controller/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var passwordAssistLabel: UILabel!
     @IBOutlet var passwordConfirmAssistLabel: UILabel!
     @IBOutlet var nameAssistLabel: UILabel!
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         idTextField.delegate = self
@@ -31,16 +31,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         textField.layer.borderWidth = 1.0
-
+        
         if textField == idTextField {
-            if textField.text!.count < 10 {
-                textField.layer.borderColor = UIColor.red.cgColor
-            }else if textField.text!.count >= 10, textField.text!.count < 20 {
-                textField.layer.borderColor = UIColor.green.cgColor
-            }
+            judgeValidID(textField)
             let newLength = textField.text!.count + string.count - range.length
             return !(newLength > 20)
-
         }
         
         if textField == passwordTextField {
@@ -61,18 +56,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    
+    func judgeValidID(_ textField: UITextField) {
+        //서버에서 아이디 가져와 중복되는지 확인해야 함
+        let text = textField.text!
+        if !isValidId(id: text) {
+            textField.layer.borderColor = UIColor.red.cgColor
+            idAssistLabel.text = "5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다."
+            idAssistLabel.textColor = UIColor.red
+            
+        }else {
+            textField.layer.borderColor = UIColor.black.cgColor
+            idAssistLabel.text = "사용 가능한 아이디입니다."
+            idAssistLabel.textColor = UIColor.green
+            
+        }
+    }
     
     @IBAction func pressNextBtn(_ sender: Any) {
         print(idTextField.text!)
     }
     
+    func isValidId(id: String) -> Bool {
+        let idRegEx = "^(?=.*[a-z])(?=.*[0-9])[a-z0-9-_]{5,20}"
+        let idTest = NSPredicate(format: "SELF MATCHES %@", idRegEx)
+        return idTest.evaluate(with: id)
+    }
     
-
-     func isValidPassword(password: String) -> Bool {
+    
+    func isValidPassword(password: String) -> Bool {
         let passwordRegEx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z0-9$@$!%*?&]{8,16}"
         let passwordTest = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
-
+        
         return passwordTest.evaluate(with: password)
     }
     
