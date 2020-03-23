b224be78a21645425b7caabe5f76b980b68504e0
moonelysian <dandyblack@naver.com>
2020-03-23 21:13:06 +0900

    Add: User 생상 함수 추가 - todo: validation 추가하기

--- BE/signup/src/main/java/com/codesquad/signup/controller/UserController.java
+++ BE/signup/src/main/java/com/codesquad/signup/controller/UserController.java
@@ -1,8 +1,20 @@
 package com.codesquad.signup.controller;
 
+import com.codesquad.signup.repository.User;
+import com.codesquad.signup.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/join")
+    public User joinUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
 }
