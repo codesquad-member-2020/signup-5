e8ae29fe9aedf960dbcfda6ff3ef115c725b7100
moonelysian <dandyblack@naver.com>
2020-03-23 20:17:44 +0900

    Add: User 생성 - user controller 추가 - user table 정의

---
+++ BE/signup/src/main/java/com/codesquad/signup/repository/User.java
@@ -0,0 +1,32 @@
+package com.codesquad.signup.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Getter
+public class User {
+
+    @Id
+    private Long id;
+
+    @Setter
+    private String userId;
+    @Setter
+    private String password;
+    @Setter
+    private String name;
+    @Setter
+    private LocalDate birthday;
+    @Setter
+    private String sex;
+    @Setter
+    private String email;
+    @Setter
+    private String phoneNumber;
+    @Setter
+    private String interest;
+
+}
---
+++ BE/signup/src/main/java/com/codesquad/signup/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.codesquad.signup.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
