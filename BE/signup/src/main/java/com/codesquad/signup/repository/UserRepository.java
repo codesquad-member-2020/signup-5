15fa6db6fecfe98140e98e6092b315dd29e580a8
moonelysian <dandyblack@naver.com>
2020-03-24 20:06:47 +0900

    Feature: 아이디 이메일 번호 중복 검사
    - 아이디 , 이메일, 번호 중복 검사
    - 유효하지 않은 형식일 때 , 중복될 때, 사용 가능할 때 3가지 경우

--- BE/signup/src/main/java/com/codesquad/signup/repository/UserRepository.java
+++ BE/signup/src/main/java/com/codesquad/signup/repository/UserRepository.java
@@ -9,4 +9,10 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     @Query("SELECT * FROM User WHERE user_id = :userId")
     Optional<User> findByUserId(String userId);
+
+    @Query("SELECT * FROM User WHERE email = :email")
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT * FROM User WHERE phone_number = :phoneNumber")
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
--- BE/signup/src/main/java/com/codesquad/signup/util/VerifyFormatUtil.java
+++ BE/signup/src/main/java/com/codesquad/signup/util/VerifyFormatUtil.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class VerifyFormatUtil {
+
     public static boolean isCorrectUserIdFormat(String userId) {
         final String userIdFormat = "^[a-z]{5,20}";
         return isCorrectFormat(userIdFormat, userId);
@@ -12,6 +13,16 @@ public class VerifyFormatUtil {
     public static boolean isCorrectPasswordFormat(String password) {
         final String passwordPFormat = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,16}";
         return isCorrectFormat(passwordPFormat, password);
+    }
+
+    public static boolean isCorrectEmailFormat(String email) {
+        final String emailFormat = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}";
+        return isCorrectFormat(emailFormat, email);
+    }
+
+    public static boolean isCorrectPhoneNumberFormat(String phone) {
+        final String phoneNumberFormat = "[0-9]{11}";
+        return isCorrectFormat(phoneNumberFormat, phone);
     }
 
     private static boolean isCorrectFormat(String pattern, String inputValue) {
