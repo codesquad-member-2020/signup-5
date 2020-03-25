dce3962bfe06d32286e5c3163a2478aaf9af40f2
sgdg <seungdeng17@gmail.com>
2020-03-25 14:57:30 +0900

    Refector. 피드백 내용 리팩토링
    - if문 분기시 가독성 향상을 위해 브레이스 제거(조건 맞지 않으면 즉시 return 시킴), 긴 조건식 sub function으로 분리

--- FE/src/js/signup/userInterests.js
+++ FE/src/js/signup/userInterests.js
@@ -5,32 +5,31 @@ import { signupData } from '../data/signupData.js';
 const tags = [];
 
 export function addInterests({ key }, tagUiWrap, signupInterests) {
-    if (key === ',') {
-        const interest = signupInterests.value.replace(/[,]/g, '');
-        signupInterests.value = '';
-        if (!interest) return checkInterests();
-        tags.push(interest);
-        return updateTag(tagUiWrap);
-    }
-    return checkInterests();
+    if (key !== ',') return checkInterests();
+    const interest = signupInterests.value.replace(/[,]/g, '');
+    signupInterests.value = '';
+    if (!interest) return checkInterests();
+    tags.push(interest);
+    return updateTag(tagUiWrap);
 }
 
 export function removeInterests({ key }, tagUiWrap, signupInterests) {
-    if (tags.length > 0 && signupInterests.value === '' && (key === 'Backspace' || key === 'Delete')) {
-        signupInterests.value = tags.pop() + ' ';
-        return updateTag(tagUiWrap);
-    }
-    return checkInterests();
+    if (!checkRemoveCondition(key, signupInterests)) return checkInterests();
+    signupInterests.value = tags.pop() + ' ';
+    return updateTag(tagUiWrap);
+}
+
+function checkRemoveCondition(key, signupInterests) {
+    if (tags.length > 0 && signupInterests.value === '' && (key === 'Backspace' || key === 'Delete')) return true;
+    return false;
 }
 
 export function removeInterestsOnClick({ target }, tagUiWrap) {
-    if (target.classList.contains('tag-ui-close')) {
-        const data = target.getAttribute('data-item');
-        const index = tags.indexOf(data);
-        tags.splice(index, 1);
-        return updateTag(tagUiWrap);
-    }
-    return checkInterests();
+    if (!target.classList.contains('tag-ui-close')) return checkInterests();
+    const data = target.getAttribute('data-item');
+    const index = tags.indexOf(data);
+    tags.splice(index, 1);
+    return updateTag(tagUiWrap);
 }
 
 export function resetTag() {
