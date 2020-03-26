9c4342a25bdc9530806516ad060fc8a37597a4ad
sgdg <seungdeng17@gmail.com>
2020-03-26 15:08:51 +0900

    Fix. state massage update시 promise가 입력되는 상황의 방어코드 작성

--- FE/src/js/util/signupUtil.js
+++ FE/src/js/util/signupUtil.js
@@ -1,6 +1,6 @@
 export async function updateState(checkCallBackFunc, target, ...additional) {
     let stateMessage = checkCallBackFunc(target.value, ...additional);
-    if (typeof stateMessage !== 'string') stateMessage = await stateMessage.then(res => res);
+    if (typeof stateMessage === 'object') stateMessage = await stateMessage.then(res => res);
     if (!stateMessage) stateMessage = '';
     target.closest('.signup-content').querySelector('.state-text').innerHTML = stateMessage;
 }
