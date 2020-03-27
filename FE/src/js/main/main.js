5fd21c0c8b4c0d5771e3d988994725c03320bd67
sgdg <seungdeng17@gmail.com>
2020-03-27 12:26:57 +0900

    Update. js webpack bundle 후 적용 babel은 추후 적용 예정

--- FE/src/js/main/main.js
+++ FE/src/js/main/main.js
@@ -1,1 +1,10 @@
-// 메인 페이지
+import { getElement, classAdd, classRemove } from '../util/commonUtil.js';
+import { FORM_RULES } from '../constants/constant.js';
+
+const userInfoBtn = getElement('.user-info-btn');
+const userInfo = getElement('.user-info');
+
+userInfoBtn.addEventListener('click', () => {
+    if (!userInfo.classList.contains(FORM_RULES.ACTIVE_KEY)) classAdd(userInfo, FORM_RULES.ACTIVE_KEY);
+    else classRemove(userInfo, FORM_RULES.ACTIVE_KEY);
+});
